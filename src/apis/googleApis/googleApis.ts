f5fb4a93ccc42cd8451de04aecca6d0731c82f0a
surrsoft <evgebar-1@mail.ru>
2025-09-07 10:20:26 +0300

    next

--- src/apis/googleApis/googleApis.ts
+++ src/apis/googleApis/googleApis.ts
@@ -4,26 +4,29 @@ const scope = 'https://www.googleapis.com/auth/spreadsheets';
 // случайная строка
 const state = '4a2bd1c6-a672-4b26-8b1c-70ef8460a279'
 const responseType = {
-  // сначала приходит authorization code
+  // для варианта когда сначала приходит authorization code
   code: 'code',
-  // сразу приходит access token
+  // для варианта когда сразу приходит access token
   token: 'token'
 }
 
 /**
  * Формирует URL на получение access token.
- * Заствляет браузер перейти на этот URL.
+ * Заставляет браузер перейти на этот URL.
  *
  * @see https://developers.google.com/identity/protocols/oauth2/web-server#creatingclient
  */
 export function gotoAuth() {
-  const redirectUriIndex = process.env.NODE_ENV === 'development' ? 0 : 1;
+  const dev = keys.web.redirect_uris[0];
+  const prod = keys.web.redirect_uris[1];
+  const redirectUri = encodeURIComponent(process.env.NODE_ENV === 'development' ? dev : prod);
+
   const url = `https://accounts.google.com/o/oauth2/v2/auth
     ?client_id=${keys.web.client_id}
     &response_type=${responseType.token}
     &state=${state}
     &scope=${scope}
-    &redirect_uri=${encodeURIComponent(keys.web.redirect_uris[redirectUriIndex])}
+    &redirect_uri=${redirectUri}
     &prompt=consent
     &include_granted_scopes=true
   `;
